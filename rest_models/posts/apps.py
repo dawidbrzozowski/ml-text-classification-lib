77628084a7fd6abafa7ed9a696d1311feb672d27
Dawid Brzozowski <dawidbrzozowski1998@gmail.com>
2020-10-27 15:58:46 +0100

    fix_rest_api for new predictions

--- rest_models/posts/apps.py
+++ rest_models/posts/apps.py
@@ -2,12 +2,16 @@ from django.apps import AppConfig
 
 import sys
 sys.path.append('/Users/dawidbrzozowski/Projects/offensive-language-semeval')
+
 from text_clsf_lib.predictors.predictor import Predictor
-from text_clsf_lib.predictors.presets import PRESETS
+from text_clsf_lib.predictors.presets import create_predictor_preset
 
 
 class PostsConfig(AppConfig):
-    preset_name = 'glove_rnn_predictor'
-    preset = PRESETS[preset_name]
+    preset = create_predictor_preset(
+        model_name='glove_rnn',
+        type_='embedding',
+        max_seq_len=200,
+        twitter_preprocessing=True)
     name = 'posts'
     predictor = Predictor(preset)
