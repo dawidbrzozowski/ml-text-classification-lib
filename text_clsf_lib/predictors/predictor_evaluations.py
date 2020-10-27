2aa9fa9a39096aa42e592ac7835efda30c75f82e
Dawid Brzozowski <dawidbrzozowski1998@gmail.com>
2020-10-27 16:00:42 +0100

    remove legacy

--- text_clsf_lib/predictors/predictor_evaluations.py
+++ text_clsf_lib/predictors/predictor_evaluations.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from text_clsf_lib.predictors.predictor import Predictor
-from text_clsf_lib.predictors.presets import PRESETS
 import numpy as np
 
 
@@ -19,10 +18,3 @@ def deep_predictor_test_on_sample(predictor: Predictor, text: str, desired_label
     std_deviation = np.std([probs_without_word[word] for word in words])
     impact_on_text = {word: (probs_without_word[word] - whole_sentence_prob)/std_deviation for word in words}
     return sorted(impact_on_text.items(), key=lambda item: item[1])
-
-
-if __name__ == '__main__':
-    preset_name = 'glove_rnn_predictor'
-    preset = PRESETS[preset_name]
-    predictor = Predictor(preset)
-    print(deep_predictor_test_on_sample(predictor, "Me as fuck the past few weeks or more. I love you all I’m just dealin"))
