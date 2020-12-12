0be36d81a04e3acd23b7f0e5a435d877cd104947
Dawid Brzozowski <dawidbrzozowski1998@gmail.com>
2020-12-12 12:48:23 +0100

    change name from rest_models to rest_api

--- rest_api/rest_api/urls.py
+++ rest_api/rest_api/urls.py
@@ -1,4 +1,4 @@
-"""rest_models URL Configuration
+"""rest_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
